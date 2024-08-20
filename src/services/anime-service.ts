4ede636a8010c14ce65118aab3a7026f3a00d8a9
Stevie Adrian <adrianstevie0@gmail.com>
2024-08-20 15:33:51 +0700

    feat: add post API service

---
+++ src/services/anime-service.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+interface animeInfo {
+  title: string;
+  episodes: number;
+  genre: string;
+  studio: string;
+  description: string;
+  rating: number;
+}
+export const handleSubmit = (prop: animeInfo) => {
+  axios.post('http://localhost:5286/api/anime', prop).then(response => {
+    console.log("Anime added successfully", response.data)
+  }).catch(error => {
+    console.error("An error occured when adding the anime!", error);
+  })
+}
